65c104a155fb56f77dda1a3a8f70f64950e03c4a
Ricardo Trejos <ricardo@signwell.com>
2023-08-12 11:24:30 -0500

    clean up blitz stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
 import styles from "src/styles/Home.module.css"
-import { UserInfo } from "../components/UserInfo"
+import { UserInfo } from "../core/components/UserInfo"
 
 const Home: BlitzPage = () => {
   return (
